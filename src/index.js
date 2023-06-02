0ed0db0e0484fa6da50df3bfb75582eea85d08f4
MrAnacin <62877260+MrAnacin@users.noreply.github.com>
2023-06-03 02:24:16 +0500

    -

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; 6.9k (gzipped: 2.7k)
+import ReactDOM from 'react-dom/client'; 513 (gzipped: 320)
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
